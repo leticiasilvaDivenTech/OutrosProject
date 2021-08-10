87df1eab60747c9f13b2f2fd9f85e9634a245b09
leticiasilvaDivenTech <leticiasilva.diventech@gmail.com>
2021-08-09 21:49:25 -0300

    Criação Casca API

---
+++ RestApiModeloDDD/RestAPiModeloDDD.Domain/Entitys/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RestAPiModeloDDD.Domain.Entitys
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public DateTime Datacadastro { get; set; }
+        public bool IsAtivo { get; set; }
+        
+    }
+}
---
+++ RestApiModeloDDD/RestAPiModeloDDD.Domain/Entitys/Produto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace RestAPiModeloDDD.Domain.Entitys
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public bool IsDisponivel { get; set; }
+
+    }
+}
